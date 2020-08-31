a00fc1e28230ddcdcf560df9bc2ac412f0777d4b
RichEwin <richardewinlambert@gmail.com>
2020-08-31 09:33:20 +0100

    Codewars - Messi goals

---
+++ messi_goals.rb
@@ -0,0 +1,10 @@
+# Use variables to find the sum of the goals Messi scored in 3 competitions
+
+# Competition       |   Goals
+# La Liga           |   43
+# Champions League	|   10
+# Copa del Rey	    |   5
+
+def total_goals(la_liga_goals, champions_league_goals, copa_del_rey_goals) 
+    la_liga_goals + champions_league_goals + copa_del_rey_goals
+end  
